9c87bf4cfe19fecf155847495a4693d0cebc3288
Yun-Yan Chi <jaiyalas@gmail.com>
2017-07-05 17:25:30 +0800

    sdl works confirmed (using special gcc-args)

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,12 +5,8 @@ import Foreign.Ptr
 import Foreign.Storable
 --
 main :: IO ()
-<<<<<<< HEAD
-main = undefined --
-=======
 main = do
     cpath <- newCString "data/powerup/export/powerup.atlas"
     ptr_Atlas <- spAtlas_createFromFile_FFI cpath nullPtr
     atlas <- peek ptr_Atlas
     putStrLn $ show atlas
->>>>>>> 15ac6e9835089150dd70fb491ff00585a2c0bd0b
